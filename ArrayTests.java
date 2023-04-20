39b8e9562fd2fdc8edc794bf75607e891600e2ee
Matthew Brownrigg <45981739+Thinkr3@users.noreply.github.com>
2023-04-19 17:40:37 -0700

    Fix ArrayExamples and Add ArrayTests

--- ArrayTests.java
+++ ArrayTests.java
@@ -9,10 +9,63 @@ public class ArrayTests {
     assertArrayEquals(new int[]{ 3 }, input1);
 	}
 
-
   @Test
   public void testReversed() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+  @Test
+  public void testTwoElementReverseInPlace() {
+    int[] input1 = {1, 2};
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{2, 1}, input1);
+  }
+
+  @Test
+  public void testTwoElementReversed() {
+    int[] input1 = {1, 2};
+    assertArrayEquals(new int[]{2, 1}, ArrayExamples.reversed(input1));
+  }
+
+  @Test
+  public void testThreeElementReverseInPlace() {
+    int[] input1 = {1, 2, 3};
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{3, 2, 1}, input1);
+  }
+
+  @Test
+  public void testThreeElementReversed() {
+    int[] input1 = {1, 2, 3};
+    assertArrayEquals(new int[]{3, 2, 1}, ArrayExamples.reversed(input1));
+  }
+
+  @Test
+  public void testSameElementReverseInPlace() {
+    int[] input1 = {6, 6, 6};
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{6, 6, 6}, input1);
+  }
+
+  @Test
+  public void testSameElementReversed() {
+    int[] input1 = {6, 6, 6};
+    assertArrayEquals(new int[]{6, 6, 6}, ArrayExamples.reversed(input1));
+  }
+
+  @Test
+  public void testFiveElementReversed() {
+    int[] input1 = {1, 2, 3, 4, 5};
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{5, 4, 3, 2, 1}, input1);
+  }
+
+  @Test
+  public void testFiveElementReverseInPlace() {
+    int[] input1 = {1, 2, 3, 4, 5};
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{5, 4, 3, 2, 1}, input1);
+  }
+
 }
